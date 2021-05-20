a6ab86213e4ffcfbcac92e6500befcd8b7246e9b
Nathan <nathangavin987@gmail.com>
2021-05-20 20:36:09 +1200

    an example of a next.js app using typescript

---
+++ nextjs-blog/pages/posts/[id].tsx
@@ -0,0 +1,51 @@
+import Head from 'next/head';
+import { GetStaticPaths, GetStaticProps } from 'next';
+
+import Layout from '../../components/layout';
+import Date from '../../components/date';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Post(
+    { postData }:
+        {
+            postData: {
+                title: string,
+                date: string,
+                contentHtml: string
+            }
+        }
+    ) {
+
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
+    );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false // handles what happens if someone goes to a posts/[id] page with an invalid id - false causes a 404 page to show
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const postData = await getPostData(params.id as string);
+    return {
+        props: {
+            postData
+        }
+    }
+}
